2b67e9e5851ee26fa2cf7c0dd1c0a1bc451d9f04
Micah Abbott <miabbott@redhat.com>
2021-03-16 17:53:49 -0400

    ci: add support for building CoreOS
    Trying to configure scripts that will allow us to build CoreOS as part
    of CI.

---
+++ ci/build-test-coreos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -xeuo pipefail
+# Prow jobs don't support adding emptydir today
+export COSA_SKIP_OVERLAY=1
+if [ $# -eq 0 ]; then
+    echo "No CoreOS config URL given"
+    exit 1
+fi
+giturl=$1; shift
+# We generate .repo files which write to the source, but
+# we captured the source as part of the Docker build.
+# In OpenShift default SCC we'll run as non-root, so we need
+# to make a new copy of the source.  TODO fix cosa to be happy
+# if src/config already exists instead of wanting to reference
+# it or clone it.  Or we could write our .repo files to a separate
+# place.
+tmpsrc=$(mktemp -d)
+cp -a /src "${tmpsrc}"/src
+# Create a temporary cosa workdir
+cd "$(mktemp -d)"
+cosa init "${giturl}" "${tmpsrc}"/src
+# Grab the rojig name of which CoreOS distro we are building
+coreos_name=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq .rojig.name)
+if "$coreos_name" == "rhcos"; then
+    # Grab the raw value of `mutate-os-release` and use sed to convert the value
+    # to X-Y format
+    ocpver=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq -r '.["mutate-os-release"]' | sed 's|\.|-|')
+    curl -L http://base-"${ocpver}"-rhel8.ocp.svc.cluster.local > src/config/ocp.repo
+fi
+cosa fetch
+cosa build
+cosa buildextend-extensions
+cosa kola --basic-qemu-scenarios
+cosa kola run 'ext.*'
+# TODO: all tests in the future, but there are a lot
+# and we want multiple tiers, and we need to split them
+# into multiple pods and stuff.
