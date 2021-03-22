3c69efe906d7af4d3b5f9469233540a6f2a888eb
Micah Abbott <miabbott@redhat.com>
2021-03-22 09:40:58 -0400

    WIP: unfinished build coreos work

--- ci/build-test-coreos.sh
+++ ci/build-test-coreos.sh
@@ -14,14 +14,11 @@ giturl=$1; shift
 # if src/config already exists instead of wanting to reference
 # it or clone it.  Or we could write our .repo files to a separate
 # place.
-tmpsrc=$(mktemp -d)
-cp -a /src "${tmpsrc}"/src
-# Create a temporary cosa workdir
 cd "$(mktemp -d)"
-cosa init "${giturl}" "${tmpsrc}"/src
+cosa init "${giturl}"
 # Grab the rojig name of which CoreOS distro we are building
-coreos_name=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq .rojig.name)
-if "$coreos_name" == "rhcos"; then
+coreos_name=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq -r .rojig.name)
+if [ "$coreos_name" == "rhcos" ]; then
     # Grab the raw value of `mutate-os-release` and use sed to convert the value
     # to X-Y format
     ocpver=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq -r '.["mutate-os-release"]' | sed 's|\.|-|')
